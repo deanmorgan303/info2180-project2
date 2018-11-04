f482fa99c389307512ec04d9bd7b32b1e3fac447
Dean Morgan <deanmorgan303@gmail.com>
2018-11-03 20:54:29 -0500

    all peice are movable

--- fifteen.js
+++ fifteen.js
@@ -9,6 +9,10 @@ window.onload= function(){
  var value ; 
  var blankx=300;
  var blanky=300;
+ var tile_pos;
+ var tile_x;
+ var tile_y;
+ console.log(c);
  
  function move(piece){ 
       x=blankx;
@@ -21,6 +25,21 @@ window.onload= function(){
 	  blankx=new_pos["left"];
 	  blanky=new_pos["top"]; 
  }   
+  function is_movable(tile){
+	    tile_pos=$(c[tile]).position();
+		tile_x=tile_pos["left"];
+		tile_y=tile_pos["top"];
+		let move_left=blankx -100;
+		let move_right=blankx +100;
+		let move_up=blanky-100;
+		let move_down=blanky+100;
+       if(tile_x == move_left && blanky==tile_y || tile_x == move_right && blanky==tile_y||tile_y == move_up||tile_y == move_down ){
+		   move(tile);
+	   } 
+	   //else if(tile_y == move_up ||tile_y == move_down ) {
+		  //move(tile); 
+	   //}
+  }
  
  
  for (var i = 0; i<c.length; i++){
@@ -54,12 +73,60 @@ window.onload= function(){
 		  y=y-100;
 	  }
    } 
-   for(var g=0;g<c.length;g++){ 
-       console.log(g);
-	   move(g);
-  
-	   }
-	   
+    
+	/*for(let f=0; f<c.length;f++){
+	 let c_change=c[f] ;
+       c_change.onmouseover=function(){
+		 c.forEach(function( item){
+				item.classList.add("movablepiece");
+				});
+	  } 
+	}*/
+	c[0].onclick=function(){
+			is_movable(0);
+		} 
+	c[1].onclick=function(){
+			is_movable(1);
+		} 
+	c[2].onclick=function(){
+			is_movable(2);
+		} 
+     c[3].onclick=function(){
+			is_movable(3);
+		} 
+    c[4].onclick=function(){
+			is_movable(4);
+		} 
+     c[5].onclick=function(){
+			is_movable(5);
+		} 
+     c[6].onclick=function(){
+			is_movable(6);
+		}  
+    c[7].onclick=function(){
+			is_movable(7);
+		}
+    c[8].onclick=function(){
+			is_movable(8);
+		} 
+    c[9].onclick=function(){
+			is_movable(9);
+		} 
+    c[10].onclick=function(){
+			is_movable(10);
+		} 
+    c[11].onclick=function(){
+			is_movable(11);
+		} 	
+    c[12].onclick=function(){
+			is_movable(12);
+		}
+    c[13].onclick=function(){
+			is_movable(13);
+		}  		
+	c[14].onclick=function(){
+			is_movable(14);
+		} 
    }
    
   
